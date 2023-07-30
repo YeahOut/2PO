e68b4353233e4fe936a1d78399430b6f7030b2da
LeeDoYun <ldu7788@naver.com>
2023-07-30 18:01:53 +0900

    chore: 사진 순서 수정

--- src/utils/BehindDetail.js
+++ src/utils/BehindDetail.js
@@ -1,54 +1,54 @@
-import Pang1 from "../assets/image/grand1.jpg";
-import Pang2 from "../assets/image/grand2.png";
-import Pang3 from "../assets/image/grand3.jpg";
-import Pang4 from "../assets/image/grand4.png";
-import Pang5 from "../assets/image/grand5.png";
-import Pang6 from "../assets/image/grand6.png";
-import OK1 from "../assets/image/child7.jpg";
-import OK2 from "../assets/image/child8.jpg";
-import OK3 from "../assets/image/child9.jpg";
-import OK4 from "../assets/image/child10.PNG";
-import OK5 from "../assets/image/animal11.png";
-import OK6 from "../assets/image/animal12.jpg";
-import Sol1 from "../assets/image/Office13.jpg";
-import Sol2 from "../assets/image/Office14.jpg";
-import Sol3 from "../assets/image/Office15.jpg";
-import Sol4 from "../assets/image/Office16.jpg";
-import Sol5 from "../assets/image/Office17.jpg";
-import Sol6 from "../assets/image/Office18.jpg"; 
+import grand1 from "../assets/image/grand1.jpg";
+import grand2 from "../assets/image/grand2.png";
+import grand3 from "../assets/image/grand3.jpg";
+import grand4 from "../assets/image/grand4.png";
+import grand5 from "../assets/image/grand5.png";
+import grand6 from "../assets/image/grand6.png";
+import child7 from "../assets/image/child7.jpg";
+import child8 from "../assets/image/child8.jpg";
+import child9 from "../assets/image/child9.jpg";
+import build10 from "../assets/image/child10.PNG";
+import animal11 from "../assets/image/animal11.png";
+import animal12 from "../assets/image/animal12.jpg";
+import build13 from "../assets/image/Office13.jpg";
+import build14 from "../assets/image/Office14.jpg";
+import hosp15 from "../assets/image/Office15.jpg";
+import hosp16 from "../assets/image/Office16.jpg";
+import hosp17 from "../assets/image/Office17.jpg";
+import hosp18 from "../assets/image/Office18.jpg"; 
 
 const ImageData = [
   {
     group: "group1",
     details: [
-      { id: "p1", name: "빵빵1", src: Pang1 },
-      { id: "p2", name: "빵빵2", src: Pang2 },
-      { id: "p3", name: "빵빵3", src: Pang3 },
-      { id: "p4", name: "빵빵4", src: Pang4 },
-      { id: "p5", name: "빵빵5", src: Pang5 },
-      { id: "p6", name: "빵빵6", src: Pang6 },
+      { id: "p1", name: "인덱스1", src: build13 },
+      { id: "p2", name: "인덱스2", src: grand2 },
+      { id: "p3", name: "인덱스3", src: animal11 },
+      { id: "p4", name: "인덱스4", src: child8 },
+      { id: "p5", name: "인덱스5", src: child9 },
+      { id: "p6", name: "빵빵6", src: hosp15 },
     ],
   },
   {
     group: "group2",
     details: [
-      { id: "o1", name: "옥지1", src: OK1 },
-      { id: "o2", name: "옥지2", src: OK2 },
-      { id: "o3", name: "옥지3", src: OK3 },
-      { id: "o4", name: "옥지4", src: OK4 },
-      { id: "o5", name: "옥지5", src: OK5 },
-      { id: "o6", name: "옥지6", src: OK6 },
+      { id: "o1", name: "옥지1", src: grand6 },
+      { id: "o2", name: "옥지2", src: hosp17 },
+      { id: "o3", name: "옥지3", src: child7 },
+      { id: "o4", name: "옥지4", src: grand3 },
+      { id: "o5", name: "옥지5", src: grand1 },
+      { id: "o6", name: "옥지6", src: animal12 },
     ],
   },
   {
     group: "group3",
     details: [
-      { id: "s1", name: "신병1", src: Sol1 },
-      { id: "s2", name: "신병2", src: Sol2 },
-      { id: "s3", name: "신병3", src: Sol3 },
-      { id: "s4", name: "신병4", src: Sol4 },
-      { id: "s5", name: "신병5", src: Sol5 },
-      { id: "s6", name: "신병6", src: Sol6 },
+      { id: "s1", name: "신병1", src: build10 },
+      { id: "s2", name: "신병2", src: grand4 },
+      { id: "s3", name: "신병3", src: hosp16 },
+      { id: "s4", name: "신병4", src: build14 },
+      { id: "s5", name: "신병5", src: hosp18 },
+      { id: "s6", name: "신병6", src: grand5 },
     ],
   },
 ];
