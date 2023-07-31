5dd745b4918e2cd66002835639fb3a2a83818810
LeeDoYun <ldu7788@naver.com>
2023-07-31 15:45:57 +0900

    feat: 기부금 전달 방식 추가

--- src/page/Throw/Throw.jsx
+++ src/page/Throw/Throw.jsx
@@ -1,5 +1,5 @@
 import { Title } from "../../components";
-import { Root } from "./styled";
+import { Root, Container } from "./styled";
 //import handIcon from "../../assets/icon/fi-rr-hand-holding-heart.png";
 
 export const Throw = () => {
@@ -9,7 +9,8 @@ export const Throw = () => {
         title="기부금 전달 방식"
         subtitle="여러분들의 소중한 기부금이 어떻게 전달되는지 알려드리겠습니다"
       />
-      
+      <Container src="/PageThrow.png">
+      </Container>
     </Root>
   );
 };
